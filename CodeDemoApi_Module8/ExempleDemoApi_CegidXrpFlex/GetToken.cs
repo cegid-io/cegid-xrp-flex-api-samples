8d36287f37aa6c43a670a72557d117dcc26e68c6
Magali Klein <mklein@cegid.com>
2020-05-18 14:23:56 +0200

    first commit

---
+++ CodeDemoApi_Module8/ExempleDemoApi_CegidXrpFlex/GetToken.cs
@@ -0,0 +1,27 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace ExempleDemoApi_CegidXrpFlex
+{
+    class GetToken
+    {
+        public static string GetOauthToken(string username, string password, string scope, string client_id, string client_secret, string authUrl)
+        {
+            var client = new RestClient(authUrl);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("cache-control", "no-cache");
+            request.AddHeader("content-type", "application/x-www-form-urlencoded");
+            string formUrlEncoding = string.Format("grant_type=password&username={0}&password={1}&scope={2}&client_id={3}&client_secret={4}", 
+                username, password, scope, client_id, client_secret);
+            request.AddParameter("application/x-www-form-urlencoded", formUrlEncoding, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            JObject json = JObject.Parse(response.Content);
+            JToken token = json.SelectToken("access_token");
+            return (string)token;
+
+        }
+    }
+}
---
+++ CodeDemoApi_Module8/ExempleDemoApi_CegidXrpFlex/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace ExempleDemoApi_CegidXrpFlex
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0];
+            string readText = File.ReadAllText(path);
+            JObject json = JObject.Parse(readText);
+
+            string apiUrl = (string)json.SelectToken("url");
+            string tokenUrl = (string)json.SelectToken("tokenUrl");
+            string username = (string)json.SelectToken("username");
+            string password = (string)json.SelectToken("password");
+            string scope = (string)json.SelectToken("scope");
+            string client_id = (string)json.SelectToken("client_id");
+            string client_secret = (string)json.SelectToken("client_secret");
+
+            string token = GetToken.GetOauthToken(username, password, scope, client_id, client_secret, tokenUrl);
+
+            XrpFlexConnection flex = new XrpFlexConnection(apiUrl, token);
+
+            // Requete 1
+            const string req1 = "Customer?$expand=MainContact,MainContact/Address,BillingContact&$select=CustomerID,CustomerName,CustomerClass,StatementCycleID,MainContact/Email,MainContact/Phone1,MainContact/Address/AddressLine1,MainContact/Address/AddressLine2,MainContact/Address/City,MainContact/Address/State,MainContact/Address/PostalCode,MainContact/DisplayName&$filter=CustomerName eq 'Hotel du Lac'";
+            Console.WriteLine(flex.executeGetRequest(req1));
+
+            // Requete 2
+            const string req2 = "Customer?$select=CustomerID,CustomerName,MainContact/Email&$expand=MainContact&$filter=CustomerID eq 'C000000004'";
+            Console.WriteLine(flex.executeGetRequest(req2));
+
+            /*// Requete 3
+            const string req3 = "Customer?$select=CustomerID,MainContact/Email&$expand=MainContact&$filter=MainContact/Email eq 'ghrum@ghrum.bis'";
+            const string param3 = "{\"CustomerName\":{\"value\":\"Gupp info\"}}";
+            Console.WriteLine(flex.executePutRequest(req3, param3));*/
+
+            Console.ReadLine();
+        }
+    }
+}
